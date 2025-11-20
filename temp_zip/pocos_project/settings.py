5d3d95c8c6e0840e30745997158c68e1d6ef278c
tonystefan <tonystefanlog@hotmail.com>
2025-11-20 10:14:02 -0300

    to_deploy5

--- temp_zip/pocos_project/settings.py
+++ temp_zip/pocos_project/settings.py
@@ -11,14 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-key-for-development-only'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
     '192.168.0.6',
     '.vercel.app',
     '.now.sh',
-    'pocos.vercel.app'
+    'pocos.vercel.app',
+    '*.vercel.app'
 ]
 
 # Adicionando os domínios temporários corretos para testes
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Configuração do Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
